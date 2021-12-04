3c24143e42c750d91ae5d66137ada8d219d780eb
Starsky <starsky.m@live.com>
2021-12-04 20:18:28 +0100

    Initial Commit

---
+++ NotesStar/NotesStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesStarApp.swift
+//  NotesStar
+//
+//  Created by star on 04.12.21.
+//
+
+import SwiftUI
+
+@main
+struct NotesStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
