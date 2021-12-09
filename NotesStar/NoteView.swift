6d722a82bb182678a450a34af284fba8aa745321
Starsky <starsky.m@live.com>
2021-12-09 20:57:34 +0100

    UITextView in SwiftUI

--- NotesStar/NoteView.swift
+++ NotesStar/NoteView.swift
@@ -9,10 +9,49 @@ import SwiftUI
 
 struct NoteView: View {
     @State var title = ""
-    @State var noteText = ""
+    @State var noteText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam"
     
     var body: some View {
-        TextEditor(text: $noteText)
+//        TextEditor(text: $noteText)
+        TextView(text: $noteText) // UITextView
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        print("Done")
+                    }
+                }
+            }
+    }
+}
+
+struct TextView: UIViewRepresentable {
+    @Binding var text: String
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UITextView {
+        let textView = UITextView()
+        return textView
+    }
+    
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.attributedText = context.coordinator.updateAttributedString()
+    }
+
+    class Coordinator: NSObject {
+        var parent: TextView
+        
+        init(_ textView: TextView) {
+            self.parent = textView
+        }
+        
+        func updateAttributedString() -> NSAttributedString {
+            let attrs = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .largeTitle)]
+            let attrString = NSAttributedString(string: parent.text, attributes: attrs)
+            return attrString
+        }
     }
 }
 
